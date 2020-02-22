6bca7f977ad61f4c6138b9bb5cfae6a7e1883b84
Santiago Guiral <santiagoguiralwe@gmail.com>
2020-02-21 19:45:09 -0500

    Plot without animation

--- animation_func.py
+++ animation_func.py
@@ -13,4 +13,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
+plt.style('dark_background')
+
+def animation(list_files):
+    for file in list_files:      
+        plot = FuncAnimation(plt.gcf(),plotting(file), interval=0000.1)
+        plt.show()
+
+def plotting(file):
+    df = pd.read_excel(file)
+    print(df.columns)
+    index = str(input("Input the name of the desired column to plot: ")) 
+    yaxis = df.loc[:,index] 
+    xaxis = np.arange(0,len(yaxis),1)
+    plt.cla()
+    plt.plot(xaxis,yaxis)
+    plt.tight_layout()
+    plt.grid()
 
--- plotr.py
+++ plotr.py
@@ -13,3 +13,14 @@ Email: egoego2509@gmail.com
 import argparse
 import animation_func as af
 
+def main():
+    parser = argparse.ArgumentParser(description="Plots live data from an excel file")
+    parser.add_argument('-f','--files',nargs='+',help="List of files to be plotted",required=True)
+    args = parser.parse_args()
+    print(args.files)
+    af.animation(args.files)
+
+if __name__ == "__main__":
+    main()
+
+
