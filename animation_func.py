edea8c7b62cfc0f87751dc8ee3f09d9ec6349f00
Santiago Guiral <santiagoguiralwe@gmail.com>
2020-02-22 12:34:27 -0500

    Plot two columns- Warning QT

--- animation_func.py
+++ animation_func.py
@@ -13,21 +13,37 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
-plt.style('dark_background')
+plt.style.use("dark_background")
 
 def animation(list_files):
-    for file in list_files:      
-        plot = FuncAnimation(plt.gcf(),plotting(file), interval=0000.1)
+    nplot = 0
+    for file in list_files:
+        nplot +=1
+        ani = FuncAnimation(plt.gcf(),plotting(file), interval=1000)
+        #ani.save('Plot_{}.mp4'.format(nplot))
         plt.show()
 
 def plotting(file):
     df = pd.read_excel(file)
     print(df.columns)
-    index = str(input("Input the name of the desired column to plot: ")) 
-    yaxis = df.loc[:,index] 
-    xaxis = np.arange(0,len(yaxis),1)
+    print('\n')
+    print('Index the names of the columns to be plotted')
+    print('If only one column is input, the plot is y-axis vs the sample lenght of the column')
+    index = str(input("Input the name of the columns to be plotted (x-axis,y-axis): "))
+    axis = index.split(',')
+    print(axis)
+    if len(axis) == 1:
+        yaxis = df.loc[:,axis[0]]
+        xaxis = np.arange(0,len(yaxis),1)
+        xlabel, ylabel = "Sample",axis[0]
+    else:
+        xaxis = df.loc[:,axis[0]]
+        yaxis = df.loc[:,axis[1]]
+        xlabel, ylabel = axis[0],axis[1]
     plt.cla()
-    plt.plot(xaxis,yaxis)
+    #plt.ion()
+    plt.plot(xaxis,yaxis,color='#ffa500')
     plt.tight_layout()
-    plt.grid()
-
+    plt.xlabel('{}'.format(xlabel))
+    plt.ylabel('{}'.format(ylabel))
+    plt.grid(color='#39ff14',linestyle=(0, (3, 10)))
