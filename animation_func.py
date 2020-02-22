c43a4899756e9b0159b394fd5fa1b9ac62ad909d
Santiago Guiral <santiagoguiralwe@gmail.com>
2020-02-21 19:35:46 -0500

    Headers for ploting files

---
+++ animation_func.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""
+Authors
+Santiago Guiral
+Email: santiagoguiralwe@gmail.com
+Emmanuel Gómez
+Email: egoego2509@gmail.com
+"""
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+
+
---
+++ plotr.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+"""
+Authors
+Santiago Guiral
+Email: santiagoguiralwe@gmail.com
+Emmanuel Gómez
+Email: egoego2509@gmail.com
+"""
+
+#Plotting live data
+
+import argparse
+import animation_func as af
+
