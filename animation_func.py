cd08450e744bbdd1bc85d539ac1bb33a685a39ac
Santiago Guiral <santiagoguiralwe@gmail.com>
2020-02-23 11:51:53 -0500

    Static plot solved - Animation working

--- animation_func.py
+++ animation_func.py
@@ -18,6 +18,42 @@ plt.style.use("dark_background")
 def animation_plt(list_files):
     nplot = 0
     for file in list_files:
+        print (pd.read_excel(file).columns)
+        print('\n')
+        print('Index the names of the columns to be plotted')
+        print('If only one column is input, the plot is y-axis vs the sample lenght of the column')
+        index = str(input("Input the name of the columns to be plotted (x-axis,y-axis): "))
+        axis = index.split(',')
+        nplot+=1
+        ani = FuncAnimation(plt.gcf(),plotter,fargs=(file,axis,nplot),interval=100)
+        #ani.save('figure{}.mp4'.format(nplot))
+        plt.show()
+
+def plotter(i,file,axis,nplot):
+    df=pd.read_excel(file)
+    if len(axis) == 1:
+        yaxis = df.loc[:,axis[0]]
+        xaxis = np.arange(0,len(yaxis),1)
+        xlabel, ylabel = "Sample",axis[0]
+    else:
+        xaxis = df.loc[:,axis[0]]
+        yaxis = df.loc[:,axis[1]]
+        xlabel, ylabel = axis[0],axis[1]
+
+    plt.cla()
+    plt.plot(xaxis,yaxis,color='#ffa500')
+    plt.tight_layout()
+    plt.xlabel('{}'.format(xlabel))
+    plt.ylabel('{}'.format(ylabel))
+    plt.grid(color='#39ff14',linestyle=(0, (3, 10)))
+    plt.savefig("figure{}.png".format(nplot))
+
+
+
+"""
+def animation_plt(list_files):
+    nplot = 0
+    for file in list_files:
         same_plot=1
         while same_plot:
             nplot+=1
@@ -34,27 +70,3 @@ def animation_plt(list_files):
                     same_plot=0
                 else:
                     wrong_key=1
-
-def plotting(file,nplot):
-    df = pd.read_excel(file)
-    print(df.columns)
-    print('\n')
-    print('Index the names of the columns to be plotted')
-    print('If only one column is input, the plot is y-axis vs the sample lenght of the column')
-    index = str(input("Input the name of the columns to be plotted (x-axis,y-axis): "))
-    axis = index.split(',')
-    if len(axis) == 1:
-        yaxis = df.loc[:,axis[0]]
-        xaxis = np.arange(0,len(yaxis),1)
-        xlabel, ylabel = "Sample",axis[0]
-    else:
-        xaxis = df.loc[:,axis[0]]
-        yaxis = df.loc[:,axis[1]]
-        xlabel, ylabel = axis[0],axis[1]
-    plt.cla()
-    plt.plot(xaxis,yaxis,color='#ffa500')
-    plt.tight_layout()
-    plt.xlabel('{}'.format(xlabel))
-    plt.ylabel('{}'.format(ylabel))
-    plt.grid(color='#39ff14',linestyle=(0, (3, 10)))
-    plt.savefig("figure{}.png".format(nplot))
